85dfdd5ff9ffc9c302b81c1be3249b6b3de05318
ciaran <ctadm66@yahoo.ie>
2017-07-07 11:48:17 +0100

    Completed greedy algorithm for initial configuration

--- greedyKnap.R
+++ greedyKnap.R
@@ -1,7 +1,7 @@
-eval<-function(conf){
+eval<-function(conf,w,v){
   Wm<-400
-  vals<-c(50,40,30,60,100,150,120,70)
-  ws<-c(50,30,60,50,50,50,50,200)
+  vals<-v
+  ws<-w
   cVal<-sum(conf*vals) #config value
   cW<-sum(conf*ws) #config weight
   lim<-Wm+mean(ws)
@@ -12,24 +12,27 @@ eval<-function(conf){
   res[3]<-ifelse(cW>Wm && cW<lim,1,0) #if the weight is greater than Wm AND less than the limit return 1 else return 0
   return(res)
 }
-
+greedy<-function(w,v){
+  vals<-v
+  ws<-w
+  rs<-vals/ws
+  ind<-sort.int(rs,decreasing=T,index.return=T)$ix
+  reOrderedWeights<-(ws[ind])
+  curW<-0
+  Wm<-400
+  conf<-numeric(8)
+  track<-1
+  while(track<=8){
+    if(curW+reOrderedWeights[track]>Wm){
+      break
+    }else{
+    curW<-curW+reOrderedWeights[track]
+    conf[ind[track]]<-1
+    track<-track+1
+    }
+  }
+  return(conf)
+}
 vals<-c(50,40,30,60,100,150,120,70)
 ws<-c(50,30,60,50,50,50,50,200)
-rs<-vals/ws
-ind<-sort.int(rs,decreasing=T,index.return=T)$ix
-reOrderedWeights<-(ws[ind])
-curW<-0
-Wm<-400
-conf<-numeric(8)
-track<-1
-while(track<=8){
-  if(curW+reOrderedWeights[track]>Wm){
-    break
-  }else{
-  curW<-curW+reOrderedWeights[track]
-  conf[ind[track]]<-1
-  track<-track+1
-  }
-}
-conf
-eval(conf)
+greedy(ws,vals)
